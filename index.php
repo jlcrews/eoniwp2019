ba86a3ff38eda013673c4f31c07286455342c91e
Zachary Smith <info@zachis.it>
2019-07-25 08:16:17 -0400

    homepage slider styling

--- index.php
+++ index.php
@@ -8,61 +8,66 @@ get_header() ?>
 <main class="wrapper">
     <div class="homeSlider">
         <ul class="slick_slider">
-            <li>
+            <li class="internet">
                 <div class="left">
-                    <h2>Title Here</h2>
-                    <p>paragraph text here</p>
+                    <h2>
+                        <span class="top">Smokin' Fast</span>
+                        <span class="bottom">High Speed Internet</span>
+                    </h2>
+                    <ul>
+                        <li>Fiber Internet & Voice</li>
+                        <li>For your home and/or business</li>
+                        <li>Speeds up to 1Gbps</li>
+                    </ul>
                     <div class="priceAction">
-                        <div class="numbers">$34.34</div>
+                        <div class="priceStats">
+                            <div class="numbers">$34.34</div>
+                            <div class="asterisks">
+                                <span class="starting">*starting at</span>
+                                <span class="month">/month</span>
+                            </div>
+                        </div>
                         <div class="learnMore">
                             <a href="<?=home_url('')?>"
                                title="Learn More"
-                               class="greenSquareButton"
                             >Learn More</a>
                         </div>
                     </div>
                 </div>
                 <div class="asset">
-                    <img src="https://dummyimage.com/300x140/634/fff.png"
-                         alt="change me"/>
+                    <img src="<?=get_template_directory_uri() ?>/images/home/monitor.png"
+                         alt="computer monitor screen orange"/>
                 </div>
             </li>
-            <li>
+            <li class="voice">
                 <div class="left">
-                    <h2>Title Here</h2>
-                    <p>paragraph text here</p>
+                    <h2>
+                        <span class="top">Crystal Clear</span>
+                        <span class="bottom">Voice Communications</span>
+                    </h2>
+                    <ul>
+                        <li>Residential voice line with unlimited local calling</li>
+                        <li>Various long distance minutes included</li>
+                        <li>Voicemail to email, call forwarding, & more!</li>
+                    </ul>
                     <div class="priceAction">
-                        <div class="numbers">$34.34</div>
+                        <div class="priceStats">
+                            <div class="numbers">$19.99</div>
+                            <div class="asterisks">
+                                <span class="starting">*starting at</span>
+                                <span class="month">/month</span>
+                            </div>
+                        </div>
                         <div class="learnMore">
                             <a href="<?=home_url('')?>"
                                title="Learn More"
-                               class="greenSquareButton"
                             >Learn More</a>
                         </div>
                     </div>
                 </div>
                 <div class="asset">
-                    <img src="https://dummyimage.com/300x140/634/fff.png"
-                         alt="change me"/>
-                </div>
-            </li>
-            <li>
-                <div class="left">
-                    <h2>Title Here</h2>
-                    <p>paragraph text here</p>
-                    <div class="priceAction">
-                        <div class="numbers">$34.34</div>
-                        <div class="learnMore">
-                            <a href="<?=home_url('')?>"
-                               title="Learn More"
-                               class="greenSquareButton"
-                            >Learn More</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="asset">
-                    <img src="https://dummyimage.com/300x140/634/fff.png"
-                         alt="change me"/>
+                    <img src="<?=get_template_directory_uri() ?>/images/home/phone.png"
+                         alt="phone in cradle"/>
                 </div>
             </li>
         </ul>
